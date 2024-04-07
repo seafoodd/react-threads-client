ab694de589d74091aee411fc6e0ad8a7afc0579d
seafood <mail.seafood@proton.me>
2024-04-07 18:55:56 +0300

    add logout button to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../theme-provider";
 import {
+  Button,
   Navbar,
   NavbarBrand,
   NavbarContent,
@@ -8,9 +9,22 @@ import {
 } from "@nextui-org/react";
 import { FaRegMoon } from "react-icons/fa";
 import { LuSunMedium } from "react-icons/lu";
+import { logout, selectIsAuthenticated } from "../../features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {CiLogout} from 'react-icons/ci';
 
 const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    localStorage.removeItem("token");
+    navigate("/auth");
+  };
 
   return (
     <div>
@@ -25,7 +39,16 @@ const Header = () => {
           >
             {theme === "dark" ? <FaRegMoon /> : <LuSunMedium />}
           </NavbarItem>
-          <NavbarItem></NavbarItem>
+          <NavbarItem>
+            {isAuthenticated && (
+              <Button
+                color="default"
+                variant={"flat"}
+                className={"gap-2"}
+                onClick={handleLogout}
+              ><CiLogout/> <span>Выйти</span></Button>
+            )}
+          </NavbarItem>
         </NavbarContent>
       </Navbar>
     </div>
